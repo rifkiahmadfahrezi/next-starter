db80182bb07703e5c5162eafa0bd2df72259715f
Rifki ahmad fahrezi <56358927+rifkiahmadfahrezi@users.noreply.github.com>
2026-09-01 14:51:57 +0700

    Enable unoptimized images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // for deployment purpose, change if needed
+  images: { 
+    unoptimized: true 
+  }
 };
 
 export default nextConfig;
